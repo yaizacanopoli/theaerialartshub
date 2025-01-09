f7e033f848c8797af808d8e6ddcf023793dce255
yaizacanopoli <yaizacanopoli@gmail.com>
2025-01-10 00:05:08 +0800

    Start aligning nav menu

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const navLogo = document.querySelector("#nav-logo");
 const mobileMenu = document.querySelector("#mobile-menu");
+const closingX = document.querySelector("#closing-x");
+const headerContainer = document.querySelector("#header-container");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
 
 navLogo.addEventListener("click", () => {
-    if (mobileMenu.style.display === "none") {
         mobileMenu.style.display = "flex";
-    } else {
-        mobileMenu.style.display = "none";
-    }
+        headerContainer.style.display = "none";
+        main.style.display = "none";
+        footer.style.display = "none";
+})
+closingX.addEventListener ("click", () => {
+    mobileMenu.style.display = "none";
+    headerContainer.style.display = "flex";
+    main.style.display = "flex";
+    footer.style.display = "flex";
 })
