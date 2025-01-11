9ae94e50c2288875f1337c7949b4d5ab36fa82ac
yaizacanopoli <yaizacanopoli@gmail.com>
2025-01-11 12:00:29 +0800

    Finish setup of home page

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const mobileMenu = document.querySelector("#mobile-menu");
 const closingX = document.querySelector("#closing-x");
 const headerContainer = document.querySelector("#header-container");
 const main = document.querySelector("main");
+const heartIcon = document.querySelectorAll("#heart-icon");
 const footer = document.querySelector("footer");
+
+// doesn't seem to work, need to test once live
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
 
 navLogo.addEventListener("click", () => {
         mobileMenu.style.display = "flex";
@@ -16,4 +22,16 @@ closingX.addEventListener ("click", () => {
     headerContainer.style.display = "flex";
     main.style.display = "flex";
     footer.style.display = "flex";
+
+    // memorise scroll position
+})
+
+heartIcon.forEach(icon => {
+    icon.addEventListener("click", () => {
+        if (icon.src.endsWith("assets/heart-outline.svg")) {
+            icon.src = "assets/heart.svg"
+        } else {
+            icon.src = "assets/heart-outline.svg"
+        }
+    })
 })
